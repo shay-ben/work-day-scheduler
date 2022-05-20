c0ec3f3bdd9a22ebbb2217cf516a7fbca78cbfc0
Shannen Bendeck <shay@shannens-mbp.myfiosgateway.com>
2022-05-20 03:16:07 -0400

    local storage

--- assets/script.js
+++ assets/script.js
@@ -8,4 +8,31 @@ $(".saveBtn").on("click", function() {
     localStorage.setItem(time, planner);
 });
 
-var present = moment().add(1, "h");
+
+
+$(".time-block").each(function() {
+    var currentHour = moment().hour();
+    var time = $(this).parent().attr("id");
+        if (time < currentHour) {
+            $(this).addClass("past");
+        } else if (time === currentHour){
+            $(this).addClass("present");
+            $(this).removeClass("past");
+        } else {$(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+    }
+ })
+
+ $("#1 .planner").val(localStorage.getItem("1"));
+ $("#2 .planner").val(localStorage.getItem("2"));
+ $("#3 .planner").val(localStorage.getItem("3"));
+ $("#4 .planner").val(localStorage.getItem("4"));
+ $("#5 .planner").val(localStorage.getItem("5"));
+ $("#6 .planner").val(localStorage.getItem("6"));
+ $("#7 .planner").val(localStorage.getItem("7"));
+ $("#8 .planner").val(localStorage.getItem("8"));
+ $("#9 .planner").val(localStorage.getItem("9"));
+ $("#10 .planner").val(localStorage.getItem("10"));
+ $("#11 .planner").val(localStorage.getItem("1"));
+ $("#12 .planner").val(localStorage.getItem("12"));
